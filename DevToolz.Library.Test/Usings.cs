a3fb1eb1b9672a15106137b23b4d63b064b47516
Lucas Zottis <zottis.lucas@hotmail.com>
2024-09-21 20:54:00 -0300

    Implementado validador de e-mail

---
+++ DevToolz.Library.Test/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using DevToolz.Library.Enums;
+global using DevToolz.Library.Extensions;
+global using DevToolz.Library.Interfaces;
+global using DevToolz.Library.Structs;
+global using System.ComponentModel;
+global using System.Globalization;
+global using System.Text.RegularExpressions;
