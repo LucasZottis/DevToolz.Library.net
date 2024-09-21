e57b1a2c2c1daaed19d092e9ac11a7c8b296bedf
Lucas Zottis <zottis.lucas@hotmail.com>
2024-09-21 15:13:14 -0300

    Criado enumerados necessários.

---
+++ DevToolz.Library/Enums/CryptProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace DevToolz.Library.Enums;
+
+/// <summary>
+/// Enumerator com os tipos de classes To criptografia.
+/// </summary>
+public enum CryptProvider
+{
+    /// <summary>
+    /// Representa a classe base To implementações criptografia dos algoritmos simétricos Rijndael.
+    /// </summary>
+    Rijndael,
+
+    /// <summary>
+    /// Representa a classe base To implementações do algoritmo RC2.
+    /// </summary>
+    RC2,
+
+    /// <summary>
+    /// Representa a classe base To criptografia de dados padrões (DES - Data Encryption Standard).
+    /// </summary>
+    DES,
+
+    /// <summary>
+    /// Representa a classe base (TripleDES - Triple Data Encryption Standard).
+    /// </summary>
+    TripleDES
+}
