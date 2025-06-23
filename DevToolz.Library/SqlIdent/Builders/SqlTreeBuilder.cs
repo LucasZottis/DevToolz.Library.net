652ae5449491b6b0becf27886da3098dac5c4bc7
Lucas Zottis <zottis.lucas@hotmail.com>
2025-06-23 12:00:25 -0300

    Feat Implementado formatador de SQL

---
+++ DevToolz.Library/SqlIdent/Builders/SqlTreeBuilder.cs
@@ -0,0 +1,59 @@
+﻿using DevToolz.Library.SqlIdent.Helpers;
+using DevToolz.Library.SqlIdent.Models;
+
+namespace DevToolz.Library.SqlIdent.Builders;
+
+internal static class SqlTreeBuilder
+{
+    /// <summary>
+    /// Preparação da formatação
+    /// </summary>
+    /// <param name="Query"></param>
+    /// <return></return>
+    private static SqlTree Parse( string query )
+    {
+        var sql = QueryHelper.PrepareQuery( query );
+        var matches = Regex.Matches( sql, @"(/\*((.|\s)*?)\*/|'((.|\s)*?)'|^(.+?)$)", RegexOptions.Multiline | RegexOptions.ExplicitCapture );
+        var trees = new Stack<SqlTree>();
+        var tree = new SqlTree();
+
+        foreach ( Match match in matches )
+        {
+            var line = match.Value.Trim( ' ', '\n', '\r', '\t' );
+
+            if ( line == string.Empty ) 
+                continue;
+            else if ( line == "(" )
+            {
+                var newTree = new ParenthesisNode();
+
+                trees.Push( tree );
+                tree.nodes.Enqueue( newTree );
+                tree = newTree.Tree;
+            }
+            else if ( line == ")" )
+            {
+                try
+                {
+                    tree = trees.Pop();
+                }
+                catch
+                {
+                    throw new FormatException( "Muitos parênteses de fechamento" );
+                }
+            }
+            else
+                tree.nodes.Enqueue( new StringNode( line ) );
+        }
+
+        if ( trees.Count > 0 )
+            throw new FormatException( "Muitos parênteses de abertura" );
+
+        return tree;
+    }
+
+    internal static SqlTree Build( string sql )
+    {
+        return Parse( sql );
+    }
+}
---
+++ DevToolz.Library/SqlIdent/Models/ParenthesisNode.cs
@@ -0,0 +1,23 @@
+﻿using DevToolz.Library.SqlIdent.Interfaces;
+
+namespace DevToolz.Library.SqlIdent.Models;
+
+internal class ParenthesisNode : ISQLTreeNode
+{
+    private SqlTree tree;
+
+    internal SqlTree Tree
+    {
+        get { return tree; }
+        set { tree = value; }
+    }
+
+    public object Content => tree;
+
+    internal ParenthesisNode( SqlTree sqlTree )
+    {
+        Tree = sqlTree;
+    }
+
+    internal ParenthesisNode() : this( new SqlTree() ) { }
+}
