e24377b36f8d5d2d8361880750b9c0b7436b6d64
Lucas Zottis <zottis.lucas@hotmail.com>
2026-03-25 19:08:28 -0300

    Removido teste unitário

--- DevToolz.Library/Documents/Brazilian/Cpf/Cpf.cs
+++ DevToolz.Library/Documents/Brazilian/Cpf/Cpf.cs
@@ -13,17 +13,17 @@ public sealed class Cpf : ICpfMetadata, IValidator, IGenerator
     private string _value = string.Empty;
 
     public bool IsValid( string value ) => _validator.IsValid( value );
-    public bool IsValid()               => _validator.IsValid( _value );
+    public bool IsValid() => _validator.IsValid( _value );
 
-    public string Generate()              => _value = _generator.Generate();
+    public string Generate() => _value = _generator.Generate();
     public string Generate( bool masked ) => _value = _generator.Generate( masked );
 
-    public string Masked()   => BrazilianDocumentHelper.MaskCpf( BrazilianDocumentHelper.RemoveMask( _value ) );
+    public string Masked() => BrazilianDocumentHelper.MaskCpf( BrazilianDocumentHelper.RemoveMask( _value ) );
     public string Unmasked() => BrazilianDocumentHelper.RemoveMask( _value );
 
-    public string               BaseDigits           => Unmasked()[..9];
-    public string               FirstVerifyingDigit  => Unmasked()[9].ToString();
-    public string               SecondVerifyingDigit => Unmasked()[10].ToString();
-    public int                  IssuingUnitDigit     => int.Parse( Unmasked()[8].ToString() );
-    public IReadOnlyList<State> IssuingStates        => StateResolver.Resolve( IssuingUnitDigit );
+    public string BaseDigits => Unmasked()[ ..9 ];
+    public string FirstVerifyingDigit => Unmasked()[ 9 ].ToString();
+    public string SecondVerifyingDigit => Unmasked()[ 10 ].ToString();
+    public int IssuingUnitDigit => int.Parse( Unmasked()[ 8 ].ToString() );
+    public IReadOnlyList<State> IssuingStates => StateResolver.Resolve( IssuingUnitDigit );
 }
